4900a5e3b4c9356c7552ace3517386729c42e574
qinzhanglove <www.sign.cn@qq.com>
2017-12-27 21:30:14 +0800

    Initial commit

---
+++ web/src/main/java/com/me/Application.java
@@ -0,0 +1,16 @@
+package com.me;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by jack rose on 2017/12/27.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
+}
