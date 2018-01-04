b7b13400d9d60b4c901579aaee994edd8b16e99b
qinzhanglove <www.sign.cn@qq.com>
2018-01-04 17:21:49 +0800

    Add files via upload

---
+++ web/src/main/java/com/me/interceptor/AppSecurityInterceptor.java
@@ -0,0 +1,48 @@
+package com.hrt.interceptor;
+
+import com.hrt.common.exception.BusinessException;
+import com.hrt.common.exception.ErrorCode;
+import com.hrt.utils.ConstantsUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AppSecurityInterceptor extends HandlerInterceptorAdapter {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(AppSecurityInterceptor.class);
+
+
+	@Override
+	public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response, Object handler) throws Exception {
+
+		String requestUri = request.getRequestURI();
+
+		String authToken = request.getHeader(ConstantsUtils.AUTH_TOKEN_HEADER_NAME);
+		if(null == authToken || authToken.equals("")){
+			throw new BusinessException(ErrorCode.AUTHTOKEN_ERROR);
+		}
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+
+		super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+
+		super.afterCompletion(request, response, handler, ex);
+	}
+}
