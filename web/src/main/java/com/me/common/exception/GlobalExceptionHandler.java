0f0744a7246c7f34d5cd811055c8d5809358276e
qinzhanglove <www.sign.cn@qq.com>
2018-01-04 21:55:26 +0800

    调整相关异常和jwt

--- web/src/main/java/com/me/common/exception/GlobalExceptionHandler.java
+++ web/src/main/java/com/me/common/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.me.common.exception;
 
+import com.me.common.result.ResultBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
