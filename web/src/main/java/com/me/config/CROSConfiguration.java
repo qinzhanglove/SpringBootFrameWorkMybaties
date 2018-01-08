c51a9c13b6aa6ca67c2f774b288f939cc9ead21d
qinzhanglove <www.sign.cn@qq.com>
2018-01-08 20:23:17 +0800

    添加跨域访问

---
+++ web/src/main/java/com/me/config/CROSConfiguration.java
@@ -0,0 +1,21 @@
+package com.me.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by jack rose on 2018/1/8.
+ */
+@Configuration
+public class CROSConfiguration extends WebMvcConfigurerAdapter {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowedOrigins("*");
+    }
+}
